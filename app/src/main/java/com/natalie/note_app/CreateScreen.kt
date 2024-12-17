f833da2d1a1b4e908fce1d51fbab1deebe88e7c1
CaseusArcus <natalie.esna@gmail.com>
2024-12-17 14:04:19 +0100

    CreateScreen.kt
    CreateScreen implemented.
    
    Uses NoteData.
    
    Navigates back to MainScreen when pushing save-button.

--- app/src/main/java/com/natalie/note_app/CreateScreen.kt
+++ app/src/main/java/com/natalie/note_app/CreateScreen.kt
@@ -1,6 +1,25 @@
 package com.natalie.note_app
 
-import androidx.navigation.NavHostController
+import androidx.compose.runtime.*
+import androidx.navigation.NavController
 
-class CreateScreen(navController: NavHostController, notes: MutableList<Note>) {
+@Composable
+fun CreateScreen(navController: NavController, notes: MutableList<Note>) {
+    var title by remember { mutableStateOf("") }
+    var text by remember { mutableStateOf("") }
+
+    NoteData(
+        title = title,
+        onTitleChange = { title = it },
+        text = text,
+        onTextChange = { text = it },
+        onSave = {
+            notes.add(Note(
+                id = System.currentTimeMillis().toString(),
+                title = title,
+                text = text
+            ))
+            navController.navigate("main")
+        }
+    )
 }
